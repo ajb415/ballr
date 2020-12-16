513b44fee656920c805e928ee78b0f2458218640
ajb415 <76086066+ajb415@users.noreply.github.com>
2020-12-15 23:31:11 -0500

    Update court_themes.R

--- court_themes.R
+++ court_themes.R
@@ -1,12 +1,12 @@
 court_themes = list(
   light = list(
-    court = '#fffcf2',
-    lines = '#999999',
-    text = '#222222',
-    made = '#00bfc4',
-    missed = '#f8766d',
-    hex_border_size = 0.3,
-    hex_border_color = "#cccccc"
+   court = '#000004',
+   lines = '#999999',
+   text = '#f0f0f0',
+   made = '#1d1060',
+   missed = '#e75826',
+   hex_border_size = 0,
+   hex_border_color = "#000000"
   ),
   dark = list(
     court = '#000004',
